9d263c7e14e999a0ed0c99c6ccbe601f04c57d1c
unknown <qoororo404@gmail.com>
2017-08-09 23:04:46 -0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+	 public static void main(String[] args) {
+       
+    	int count = 0;
+    	while (count < 10) {
+    		System.out.println(count);
+    		count++;
+    	}
+    }
+}
